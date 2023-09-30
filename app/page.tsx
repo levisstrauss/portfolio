b28f9907912f51a7f80c0f6f0ba9bcafc9b46534
Zakaria Coulibaly <kategnimincoulibaly@MacBook-Pro.local>
2023-09-30 01:48:52 -0400

    Portfolio

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,193 @@
-import Image from 'next/image'
+"use client";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faLinkedin, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
+import gsap from 'gsap';
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import '../tailwind.css';
+
+// @ts-ignore
+import vertexShader from "./shaders/vertex.glsl";
+// @ts-ignore
+import fragmentShader from "./shaders/fragment.glsl";
+
+// @ts-ignore
+import atmosphereVertexShader from "./shaders/atmosphereVertex.glsl";
+// @ts-ignore
+import atmosphereFragmentShader from "./shaders/atmosphereFragment.glsl";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+
+    const canvasContainerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const canvasContainer = document.querySelector('#canvasContainer');
+        const scene = new THREE.Scene();
+
+        // @ts-ignore
+        const camera = new THREE.PerspectiveCamera(75, canvasContainer?.offsetWidth / canvasContainer?.offsetHeight, 0.1, 1000);
+
+        const renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            // @ts-ignore
+            canvas: document.querySelector('canvas'),
+        });
+
+
+        // @ts-ignore
+        renderer.setSize(canvasContainer.offsetWidth, canvasContainer.offsetHeight);
+        renderer.setPixelRatio(window.devicePixelRatio)
+
+        // Create a sphere
+        const sphere = new THREE.Mesh(
+            new THREE.SphereGeometry(5, 50, 50),
+            new THREE.ShaderMaterial({
+                vertexShader,
+                fragmentShader,
+                uniforms: {
+                    globeTexture: {
+                        value: new THREE.TextureLoader().load('./img/globes.jpg')
+                    }
+                }
+            })
+        );
+
+        const handleMouseMove = (event: MouseEvent) => {
+            mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+            mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+        };
+
+        if (canvasContainerRef.current) {
+            canvasContainerRef.current.addEventListener('mousemove', handleMouseMove);
+        }
+
+
+        // Create an atmosphere
+        const atmosphere = new THREE.Mesh(
+            new THREE.SphereGeometry(5, 50, 50),
+            new THREE.ShaderMaterial({
+                vertexShader: atmosphereVertexShader,
+                fragmentShader: atmosphereFragmentShader,
+                blending: THREE.AdditiveBlending,
+                side: THREE.BackSide
+            })
+        );
+
+        atmosphere.scale.set(1.1, 1.1, 1.1);
+
+        scene.add(atmosphere);
+
+        const group = new THREE.Group();
+        group.add(sphere)
+        scene.add(group)
+
+        const starGeometry = new THREE.BufferGeometry();
+        const starMaterial = new THREE.PointsMaterial({
+            color: 0xffffff
+        });
+
+        const starsVertices = []
+        for (let i = 0; i < 10000; i++){
+           const x = (Math.random() - 0.5) * 2000;
+           const y = (Math.random() - 0.5) * 2000;
+           const z = -Math.random() * 3000;
+           starsVertices.push(x, y, z);
+        }
+
+        starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(
+            starsVertices, 3));
+
+        const stars = new THREE.Points( starGeometry, starMaterial);
+        scene.add(stars)
+
+        camera.position.z = 15;
+
+
+        // @ts-ignore
+        function createPoint(lat, lng) {
+            const point = new THREE.Mesh(
+                new THREE.SphereGeometry(0.1, 50, 50),
+                new THREE.MeshBasicMaterial({
+                    color: '#ff0000'
+                })
+            );
+
+            const latitude = (lat /180) * Math.PI;
+            const longitude = (lng /180) * Math.PI;
+            const radius = 5;
+
+            const x = radius * Math.cos(latitude) * Math.sin(longitude)
+            const y = radius * Math.sin(latitude)
+            const z = radius * Math.cos(latitude) * Math.cos(longitude)
+
+            point.position.x = x
+            point.position.y = y
+            point.position.z = z
+
+            group.add(point);
+        }
+
+        createPoint(23.6345, -102.5528);
+        createPoint(-14.235, -51.9253);
+        createPoint(7.539989, -5.54708);
+        createPoint(39.3812669989, -97.922211);
+        createPoint(37.0902, 95.7129);
+
+        sphere.rotation.y = -Math.PI / 2;
+
+        const mouse = { x: 0, y: 0 };
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            renderer.render(scene, camera);
+            sphere.rotation.y += 0.003;
+            gsap.to(group.rotation, {
+                x: -mouse.y * 1.8,
+                y: mouse.x * 1.8,
+                duration: 2
+            })
+        };
+        animate();
+    }, []);
+
+    return (
+        <div className='flex h-screen bg-black'>
+            <div className="w-1/2 flex flex-col justify-center px-8">
+                <div style={{maxWidth:500}} className='mx-auto'>
+                   <h1 className='text-white text-4xl mb-8 leading-none'>HI AND WELCOME TO MY PORTFOLIO</h1>
+                    <p className='text-gray-400 mb-8'>
+                        Computer Science enthusiast with a fervent passion for
+                        bridging the realms of Artificial Intelligence and Software
+                        Engineering. I am driven by the challenge of leveraging AI to
+                        engineer solutions that matter, aspiring always to push the
+                        boundaries of what software can achieve. My journey in this
+                        dynamic field has been marked by rigorous academic pursuits,
+                        hands-on projects, and a ceaseless desire to learn and innovate.
+                        Let's harness the potential of technology, together.
+                    </p>
+                    <p className='text-white'>Want to learn more about me, navigate the globe!</p>
+                    <div className="flex justify-start mt-4 space-x-4" >
+                        <a href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer" title="Instagram">
+                            <FontAwesomeIcon icon={faInstagram} className="text-xl text-white hover:text-gray-400" />
+                        </a>
+                        <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer" title="LinkedIn">
+                            <FontAwesomeIcon icon={faLinkedin} className="text-xl text-white hover:text-gray-400" />
+                        </a>
+                        <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer" title="Twitter">
+                            <FontAwesomeIcon icon={faTwitter} className="text-xl text-white hover:text-gray-400" />
+                        </a>
+                        <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer" title="GitHub">
+                            <FontAwesomeIcon icon={faGithub} className="text-xl text-white hover:text-gray-400" />
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <div ref={canvasContainerRef}  className="w-1/2" id="canvasContainer">
+                <canvas></canvas>
+            </div>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
+//
+// module.exports = nextConfig
+module.exports = {
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.glsl$/,
+            use: 'raw-loader',
+        });
 
-module.exports = nextConfig
+        return config;
+    },
+};
