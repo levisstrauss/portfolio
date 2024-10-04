060177d08d4389e9ca832a33d7b9f6543db87507
levisstrauss <levisstrauss11@yahoo.com>
2024-10-04 05:06:43 -0400

    Resume section implemented

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -3,6 +3,11 @@
 
 import { FaHtml5, FaCss3, FaJs, FaReact, FaFigma, FaNodeJs} from "react-icons/fa";
 import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
+import { ScrollArea} from "@/components/ui/scroll-area";
+import { motion } from "framer-motion";
+
 
 {/*------------- About data ---------------------*/}
 
@@ -28,7 +33,19 @@ const about = {
         },
         {
             fieldName: "Languages",
-            fieldValue: "English - French"
+            fieldValue: "English, French"
+        },
+        {
+            fieldName: "Twitter",
+            fieldValue: "Levis@2021"
+        },
+        {
+            fieldName: "Opportunity",
+            fieldValue: "Available"
+        },
+        {
+            fieldName: "Youtube",
+            fieldValue: "Codemon"
         },
     ]
 }
@@ -171,11 +188,6 @@ const skills = {
 }
 
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "@/components/ui/tooltip";
-import { ScrollArea} from "@/components/ui/scroll-area";
-import { motion } from "framer-motion";
-
 const Resume = () => {
     return (
         <motion.div
@@ -288,8 +300,21 @@ const Resume = () => {
                             </div>
                         </TabsContent>
                         {/*------------- About ---------------------*/}
-                        <TabsContent value="about" className="w-full">
-                            About Me
+                        <TabsContent value="about" className="w-full text-center xl:text-left">
+                            <div className="flex flex-col gap-[30px]">
+                                <h3 className="text-4xl font-bold">{about.title}</h3>
+                                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">{about.description}</p>
+                                <ul className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 max-w-[620px] mx-auto xl:mx-0">
+                                    {about.info.map((item, index) => {
+                                        return (
+                                            <li key={index} className=" flex items-center justify-center xl:justify-start gap-4">
+                                                <span className="text-white/60">{item.fieldName}</span>
+                                                <span className="text-1xl">{item.fieldValue}</span>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
                         </TabsContent>
                     </div>
                 </Tabs>
