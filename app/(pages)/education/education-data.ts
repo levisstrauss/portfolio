72ba519c643a6265cfa767d5164c10860d60045c
Codemon <codemon@Codemons-MacBook-Pro.local>
2025-04-01 16:57:39 -0400

    Fixed content

--- app/(pages)/education/education-data.ts
+++ app/(pages)/education/education-data.ts
@@ -12,26 +12,26 @@ export const educationData: EducationType[] = [
         period: '2025 - 2026',
         color: 'orange',
         achievements: [
-            { description: 'Minor: Mathematics' },
-            { description: 'Societies: NSLS, UPE, ACM' },
-            { description: 'Dean\'s List for some semesters' }
+            // { description: 'Minor: Mathematics' },
+            // { description: 'Societies: NSLS, UPE, ACM' },
+            // { description: 'Dean\'s List for some semesters' }
         ],
         courses: [
-            {name:  'Introduction to Programming'},
-            {name:  'Object-Oriented Programming'},
-            {name:  'Discrete Mathematics'},
-            {name:  'Programming Language Concepts'},
-            { name: 'Data Structures & Algorithms' },
-            { name: 'Web Development'},
-            { name: 'Calculus'},
-            { name: 'Linear Algebra'},
-            { name: 'Software Engineering' },
-            { name: 'Database Systems' },
-            { name: 'Operating Systems' },
-            { name: 'Compilers' },
-            { name: 'Probability'}
+            // {name:  'Introduction to Programming'},
+            // {name:  'Object-Oriented Programming'},
+            // {name:  'Discrete Mathematics'},
+            // {name:  'Programming Language Concepts'},
+            // { name: 'Data Structures & Algorithms' },
+            // { name: 'Web Development'},
+            // { name: 'Calculus'},
+            // { name: 'Linear Algebra'},
+            // { name: 'Software Engineering' },
+            // { name: 'Database Systems' },
+            // { name: 'Operating Systems' },
+            // { name: 'Compilers' },
+            // { name: 'Probability'}
         ],
-        projects: ' Led team of 2 to build a precalculus learning mobile applications for helping students to understand math.',
+        projects: ' ',
     },
     {
         id: 'Bachelor',
