287c65ee57418e332df59000fc872c0a6e360b50
levisstrauss <levisstrauss11@yahoo.com>
2025-12-15 17:31:33 -0500

    Add skills section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { HeroSection } from "@/components/hero-section"
 import { AboutSection } from "@/components/about-section"
 import { ExperienceSection } from "@/components/experience-section"
 import { ProjectsSection } from "@/components/projects-section"
+import { SkillsSection } from "@/components/skills-section"
 import { Footer } from "@/components/footer"
 
 
@@ -15,6 +16,7 @@ export default function Home() {
           <AboutSection />
           <ExperienceSection />
           <ProjectsSection />
+          <SkillsSection />
 
           <Footer />
       </main>
